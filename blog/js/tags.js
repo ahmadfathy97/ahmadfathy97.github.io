22222f5ea63d4541abdb170302d572df25a1f792
ahmadfathy97 <af1381@fayoum.edu.eg>
2021-02-16 05:40:32 +0200

    added twitter card

--- blog/js/tags.js
+++ blog/js/tags.js
@@ -9,15 +9,16 @@ fetch('https://ahmad-fathy-blog.herokuapp.com/api/posts/tags')
   console.log(err);
 });
 let tagsContainer = document.getElementById('tags-container');
-
+// let colors = ['#5B2C6F', '#2E4053', '#21618C', '#D35400', '#241780', '#1B4F72'];
 function appendTags(tags){
   tagsContainer.innerHTML = '';
   tags.forEach(tag =>{
-    tagsContainer.innerHTML += htmlTags(tag);
+    tagsContainer.innerHTML += htmlTags(tag /*, colors[Math.floor(Math.random() * colors.length)]*/);
   })
 }
 
-function htmlTags(tag){
+function htmlTags(tag/*, color*/){
+  /*style="background:${color}"*/
   return `
     <a href="/blog/tag?name=${tag}" class="post-tag">#${tag}</a>
   `
