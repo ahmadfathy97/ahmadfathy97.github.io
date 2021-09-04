a7050733c94a6f4970841d87eb7b2f64ce14d6eb
ahmadfathy97 <af1381@fayoum.edu.eg>
2021-09-04 10:41:56 +0200

    added the number of posts in the tag

--- blog/js/tags.js
+++ blog/js/tags.js
@@ -1,4 +1,4 @@
-fetch('https://ahmad-fathy-blog.herokuapp.com/api/posts/tags')
+fetch('http://127.0.0.1:4000/api/posts/tags')
 .then(res => res.json())
 .then(data => {
   if(data.success){
@@ -12,14 +12,17 @@ let tagsContainer = document.getElementById('tags-container');
 // let colors = ['#5B2C6F', '#2E4053', '#21618C', '#D35400', '#241780', '#1B4F72'];
 function appendTags(tags){
   tagsContainer.innerHTML = '';
-  tags.forEach(tag =>{
-    tagsContainer.innerHTML += htmlTags(tag /*, colors[Math.floor(Math.random() * colors.length)]*/);
-  })
+  for (const [key, value] of Object.entries(tags)) {
+    tagsContainer.innerHTML += htmlTags(key, value /*, colors[Math.floor(Math.random() * colors.length)]*/);
+  }
+  // tags.forEach(tag =>{
+  //   tagsContainer.innerHTML += htmlTags(tag /*, colors[Math.floor(Math.random() * colors.length)]*/);
+  // })
 }
 
-function htmlTags(tag/*, color*/){
+function htmlTags(key, value/*, color*/){
   /*style="background:${color}"*/
   return `
-    <a href="/blog/tag?name=${tag}" class="post-tag">#${tag}</a>
+    <a href="/blog/tag?name=${key}" class="post-tag">#${key} (${value})</a>
   `
 }
